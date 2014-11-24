a9db6c72159603a948d4f3862ae20a444a315ad7
Steve Ash <stevemash@gmail.com>
2014-11-24 11:27:10 -0600

    updating enrichment to word wrap

--- bushwhacker/src/main/java/com/github/steveash/bushwhacker/MessageEnricher.java
+++ bushwhacker/src/main/java/com/github/steveash/bushwhacker/MessageEnricher.java
@@ -6,6 +6,7 @@ import com.google.common.base.Strings;
 import com.github.steveash.bushwhacker.util.SerializationUtil;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
 
 /**
  * Knows how to construct the "enriched" messages and update the existing detail Messages
@@ -19,7 +20,8 @@ public class MessageEnricher {
   private static final SerializationUtil.FieldSetter<Throwable> detailMessageSetter =
       SerializationUtil.getFieldSetter(Throwable.class, "detailMessage");
 
-  private static final int bannerSize = 75;
+  private static final int BANNER_SIZE = 77;
+  private static final int WRAP_LENGTH = 77;
 
   public static String createMessage(String original, String updateMessage,
                                      String exceptionMatched) {
@@ -29,19 +31,20 @@ public class MessageEnricher {
 
     StringBuilder sb = new StringBuilder();
     sb.append("\n");
-    sb.append(Strings.repeat("*", bannerSize));
+    sb.append(Strings.repeat("*", BANNER_SIZE));
     sb.append('\n');
     sb.append("  Bushwhacker matched an exception ").append(exceptionMatched);
     sb.append(" and wants to help you try\n  and diagnose your problem").append('\n');
     sb.append('\n');
     if (original != null) {
-      sb.append("  The original (matching) problem was: " + original).append('\n');
+      sb.append("  The original (matching) problem was: ").append('\n');
+      sb.append(WordUtils.wrap(original, WRAP_LENGTH)).append('\n');
       sb.append('\n');
     }
     sb.append("  The following hint is provided to help you:").append('\n');
-    sb.append("    ").append(updateMessage).append('\n');
+    sb.append("    ").append(WordUtils.wrap(updateMessage, WRAP_LENGTH)).append('\n');
     sb.append('\n');
-    sb.append(Strings.repeat("*", bannerSize));
+    sb.append(Strings.repeat("*", BANNER_SIZE));
 
     return sb.toString();
   }
